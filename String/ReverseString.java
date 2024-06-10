bfded6ee96f7f4579b58e8251c6e2b10a04e67d5
ankitstar-2005 <ankitgodbole90@gmail.com>
2024-06-10 15:25:42 +0530

    new string folder added

---
+++ String/ReverseString.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ReverseString {
+
+    public static String reverseString(String str ){
+        int n = str.length();
+        String reverseString  = "";
+        for(int i = n-1; i >= 0; i--){
+            reverseString += str.charAt(i);
+
+        }
+        return reverseString;
+    
+    }
+    public static void main(String[] args){
+
+        Scanner sr = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str =  sr.nextLine();
+        String reverseString = reverseString(str);
+        System.out.println(reverseString);
+
+    }
+    
+}
